4bae1cb874464377feb0f61dd5e7c496b34e59dc
Unknown <treaclywheat@gmail.com>
2017-03-12 21:16:31 -0400

    add comments, do some code cleanup

--- nupack_calculator.rb
+++ nupack_calculator.rb
@@ -3,22 +3,30 @@ require 'pry'
 class NupackCalculator
   MATERIAL_MARKUPS = { 'food' => 0.13, 'drugs' => 0.075 }
 
+  # Calculate the markup for a product based on base price, number of workers
+  # needed, and package type
+  # Assumes that the base price can not be <= 0 and that there needs to be
+  # at least one worker assigned
   def calculate_markup(base_price, number_of_workers, package_type)
-    fail ArgumentError, 'Invalid base price' if !base_price.is_a?(Numeric) || base_price < 1
-    fail ArgumentError, 'Invalid number of workers' if !number_of_workers.is_a?(Numeric) || number_of_workers < 1
+    fail ArgumentError, 'Invalid base price' if invalid_numeric_input?(base_price)
+    fail ArgumentError, 'Invalid number of workers' if invalid_numeric_input?(number_of_workers)
 
     flat_fee = 1.05
     base_price_with_flat_markup = base_price * flat_fee
-
     worker_markup = base_price_with_flat_markup * number_of_workers * 0.012
-
-    if MATERIAL_MARKUPS.include?(package_type)
-      material_markup = base_price_with_flat_markup * MATERIAL_MARKUPS[package_type]
-    else
-      material_markup = 0.0
-    end
+    material_markup = if MATERIAL_MARKUPS.include?(package_type)
+                        base_price_with_flat_markup * MATERIAL_MARKUPS[package_type]
+                      else
+                        0.0
+                      end
 
     total_fee = (base_price_with_flat_markup + worker_markup + material_markup)
     return total_fee.round(2)
+  end
+
+  private
+
+  def invalid_numeric_input?(input)
+    return (!input.is_a?(Numeric) || input < 1)
   end
 end
